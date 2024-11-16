b62ac71929887cda9b8ceebe810d889ca4c14979
Zhang-Henry <zhanghr_harris@163.com>
2024-11-16 01:37:14 -0500

    mixed attack more exp done

--- scripts/res_retrieval.py
+++ scripts/res_retrieval.py
@@ -2,8 +2,8 @@ import pandas
 import csv
 import os
 
-prompt_injections=['DPI_OPI'] # 'direct_prompt_injection',"observation_prompt_injection",'mixed_attack',"memory_attack","clean",'DPI_MP','OPI_MP','DPI_OPI'
-dirs = ['no_memory'] # new_memory, no_memory, direct_paraphrase_defense, instructional_prevention, delimiters_defense, ob_sandwich_defense
+prompt_injections=['DPI_MP'] # 'direct_prompt_injection',"observation_prompt_injection",'mixed_attack',"memory_attack","clean",'DPI_MP','OPI_MP','DPI_OPI'
+dirs = ['new_memory'] # new_memory, no_memory, direct_paraphrase_defense, instructional_prevention, delimiters_defense, ob_sandwich_defense
 agg_result = []
 non_agg_result = []
 
